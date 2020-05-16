3390120b370436200ed600450af37ac4a8b84bd1
fzw <1024779191@qq.com>
2020-05-16 13:47:35 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,117 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import AppIndex from '../components/home/AppIndex'
+import Articles from '../components/jotter/Articles'
+
+import LibraryIndex from '../components/library/LibraryIndex'
+import Login from '../components/Login'
+import Home from '../components/Home'
+
+import Register from '../components/Register'
+
+import ArticleDetails from '../components/jotter/ArticleDetails'
+
+Vue.use(Router)
+
+export default new Router({
+  //mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Default',
+      redirect: '/home',
+      component: Home
+    },
+    {
+      // home页面并不需要被访问，只是作为其它组件的父组件
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex
+        },
+        {
+          path: '/jotter',
+          name: 'Jotter',
+          component: Articles
+        },
+        {
+          path: '/jotter/article',
+          name: 'Article',
+          component: ArticleDetails
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
+  ]
+})
+
+// 用于创建默认路由
+export const createRouter = routes => new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Default',
+      redirect: '/home',
+      component: Home
+    },
+    {
+      // home页面并不需要被访问，只是作为其它组件的父组件
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex
+        },
+        {
+          path: '/jotter',
+          name: 'Jotter',
+          component: Articles
+        },
+        {
+          path: '/jotter/article',
+          name: 'Article',
+          component: ArticleDetails
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
+  ]
+})
